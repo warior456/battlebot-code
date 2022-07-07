d6cb02a7688e9b2be7bfe876a5b667e40e3db01c
warior456 <matteo.feyaerts@gmail.com>
2022-07-07 16:44:51 +0200

    work

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ lastKey = "none"
 keyList = {'z': False, 's': False, 'q': False, 'd': False, 'speed': speed_var}  #vooruit, achteruit, links, rechts, speed
 keyChange = ""
 
+packet_delay = 240
+off = False
 
 def onKeyDown(event):
     global keyDown, lastKey, keyList
@@ -41,94 +43,87 @@ def onKeyUp(event):
 
 def onTimer():
     global keyList, keyChange, timerhandle, resend_var
+    if off == False:
+        if keyChange != keyList:
+            keyChange = deepcopy(keyList)
+            resend_var = 2
+        packet.send_packet(keyList)
 
-    if keyChange != keyList:
-        keyChange = deepcopy(keyList)
-        resend_var = 2
-    # print(keyList['speed'])
-    # print(speed_var)
-    packet.send_packet(keyList)
-    # if resend_var >= 1:
-    #
-    #     print(resend_var)
-    #     resend_var -= 1
-    #     print(resend_var)
-
-    timerhandle = root.after(250, onTimer)
-
-
-# def forward(keyList):
-#     packet.button_write(keyList, 'z', button_time)
-#     return
-#
-#
-# def backward(keyList):
-#     packet.button_write(keyList, 's', button_time)
-#     return
-#
-#
-# def left(keyList):
-#     packet.button_write(keyList, 'q', button_time)
-#     return
-#
-#
-# def right(keyList):
-#     packet.button_write(keyList, 'd', button_time)
-#     return
-
+    timerhandle = root.after(packet_delay , onTimer)
 
 def send_command():
-    ping = ping_val.get()
-    ping_entry.config(state=DISABLED)
+    ping = command_val.get()
+    command_entry.config(state=DISABLED)
     command_send_btn.config(state=DISABLED)
-    pong = packet.send_raw(ping)
-
+    packet.read()
+    pong = packet.send_command(ping)
+    print(pong)
     ping_resp.config(text=pong)
-    ping_entry.config(state=NORMAL)
+    command_entry.config(state=NORMAL)
     command_send_btn.config(state=NORMAL)
-
     return
 
-def speed(speed_var):
-    global speed_msg
-    speed_msg = "speed: "
+def on_off():
+    global off
+    if off == True:
+        on_off_btn.config(text="turn control packets off")
+        command_entry.config(state=DISABLED)
+        command_send_btn.config(state=DISABLED)
+        off = False
+    elif off == False:
+        on_off_btn.config(text="turn control packets on")
+        command_entry.config(state=NORMAL)
+        command_send_btn.config(state=NORMAL)
+        off = True
+
+def motor_speed(speed_var):
+    speed_msg = "motor speed: "
     keyList['speed'] = speed_var
     speed_msg += speed_var
-    speed_label.config(text=speed_msg)
+    motor_speed_label.config(text=speed_msg)
+
+def packet_speed(delay):
+    global  packet_delay
+    packet_delay = delay
+    packet_speed_label.config(text='packet delay: ' + packet_delay + 'ms')
     return
 
-
 # buttons
-# ttk.Button(frame, text="forwards", command=forward(keyList)).grid(column=1, row=0)
-# ttk.Button(frame, text="backwards", command=backward(keyList)).grid(column=1, row=2)
-# ttk.Button(frame, text="left", command=left(keyList)).grid(column=0, row=1)
-# ttk.Button(frame, text="right", command=right(keyList)).grid(column=2, row=1)
-command_send_btn = Button(frame, text="ok", command=send_command)
-command_send_btn.grid(column=4, row=1)
+on_off_btn = Button(frame, text="turn control packets off", command= on_off)
+on_off_btn.grid(column=2, row=1)
+command_send_btn = Button(frame, text="ok", command=send_command, state=DISABLED)
+command_send_btn.grid(column=1, row=1)
 
 # labels
 ping_label = Label(frame, text="Send a command")
-ping_label.grid(column=3, row=0)
-ping_resp = Label(frame, text="No commands sent yet")
-ping_resp.grid(column=3, row=2)
+ping_label.grid(column=0, row=0)
+ping_resp = Label(frame, text="No commands sent yet\n")
+ping_resp.grid(column=0, row=2)
 
-speed_label = Label(frame, text="speed: ")
-speed_label.grid(column=0, row=3)
+motor_speed_label = Label(frame, text="motor speed: ")
+motor_speed_label.grid(column=0, row=3)
+
+packet_speed_label = Label(frame, text="packet delay: ")
+packet_speed_label.grid(column=2, row=3)
 
 # sliders
-speed_scale = Scale(frame, length=100, orient=VERTICAL, command=speed, from_=50, to=255)
-speed_scale.grid(column=0, row=4)
-speed_scale.set(speed_var)
+packet_speed_scale = Scale(frame, length=100, orient=VERTICAL, command=packet_speed, from_=150, to=1000)
+packet_speed_scale.grid(column=2, row=4)
+packet_speed_scale.set(packet_delay)
+motor_speed_scale = Scale(frame, length=100, orient=VERTICAL, command=motor_speed, from_=50, to=255)
+motor_speed_scale.grid(column=0, row=4)
+motor_speed_scale.set(speed_var)
 
-#
-ping_val = StringVar()
-ping_entry = Entry(frame, textvariable=ping_val, state=DISABLED)
-ping_entry.grid(column=3, row=1)
+#text
+command_val = StringVar()
+command_entry = Entry(frame, textvariable=command_val, state=DISABLED)
+command_entry.grid(column=0, row=1)
 
 # keybinds
 root.bind("<KeyPress>", onKeyDown)
 root.bind("<KeyRelease>", onKeyUp)
-timerhandle = root.after(250,onTimer)
+timerhandle = root.after(packet_delay ,onTimer)
+
 
 root.mainloop()
 
--- packet.py
+++ packet.py
@@ -1,25 +1,34 @@
 import serial
 import time
+import asyncio
 
-arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+arduino = serial.Serial(port='/dev/ttyACM1', baudrate=9600, timeout=.1)
 
 
 # vooruit, achteruit, links, rechts, speed
 def send_packet(keyList):
     packet_data = true_false(keyList['z'])
-    packet_data += ","
     packet_data += true_false(keyList['s'])
-    packet_data += ","
     packet_data += true_false(keyList['q'])
-    packet_data += ","
     packet_data += true_false(keyList['d'])
-    packet_data += ","
     packet_data += true_false(keyList['speed'])
     return send_raw(packet_data)
+
 
 def send_raw(packet_data):
     print(packet_data)
     write(packet_data)
+    #read()
+    return read()
+
+def send_command(packet_data):
+    print(packet_data)
+    write(packet_data)
+    read()
+    time.sleep(0.25)
+    write(packet_data)
+    read()
+    time.sleep(0.25)
     return read()
 
 def true_false(x):
@@ -29,10 +38,11 @@ def true_false(x):
         x = 1
     return str(x)
 
+
 def write(x):
     arduino.write(bytes(x, 'utf-8'))
-
     return
+
 
 def read():
     data = arduino.readline().decode('utf-8').rstrip()
